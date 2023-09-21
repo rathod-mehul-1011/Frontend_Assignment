f9f251b38eea4ebe811b1a64855390b5432cc26d
Mehul Rathod <rathodmehul1011@gmail.com>
2023-09-21 09:25:12 +0530

    modal with transition, font & color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mercury: "#E6E6E6",
+        "mine-shaft": "#212121",
+        alabaster: "#FAFAFA",
+        roman: "#D86161",
+        boulder: "#7A7A7A",
+        "curious-blue": "#1597E4",
+      },
+    },
   },
   plugins: [],
-}
+};
