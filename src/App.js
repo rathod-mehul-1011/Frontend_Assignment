bdd7658a09d41c57bf3ec21b0079936302cfe23d
Mehul Rathod <rathodmehul1011@gmail.com>
2023-09-24 17:28:16 +0530

    loading while fetching data

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ const App = () => {
   const { setIsModal1Open } = useModal();
 
   const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleJobDeleted = async (jobId) => {
     setJobs((currentJobs) => currentJobs.filter((job) => job.id !== jobId));
   };
 
   const getJobs = async () => {
+    if (loading) return;
+    setLoading(true);
     try {
       const response = await axios.get(`${process.env.REACT_APP_API_URL}/jobs`);
       if (response.status === 200) {
@@ -26,6 +29,7 @@ const App = () => {
     } catch (error) {
       toast.error("Failed to fetch jobs");
     }
+    setLoading(false);
   };
   useEffect(() => {
     getJobs();
@@ -38,6 +42,11 @@ const App = () => {
         <Button className="filled_btn" onClick={() => setIsModal1Open(true)}>
           Create Job
         </Button>
+        {loading && (
+          <div className="flex justify-center items-center h-[calc(100vh-100px)]">
+            <h1 className="text-xl font-medium text-black">Loading...</h1>
+          </div>
+        )}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-[83.118px]">
           {jobs.map((job) => (
             <JobCard key={job.id} job={job} onJobDeleted={handleJobDeleted} />
--- src/components/modals/CreateJobStep2.js
+++ src/components/modals/CreateJobStep2.js
@@ -16,7 +16,7 @@ const CreateJobStep2 = ({ getJobs }) => {
     updateFormData,
     formData,
     currentJob,
-    setCurrentJob
+    setCurrentJob,
   } = useModal();
 
   const experienceMinRef = useRef();
@@ -57,7 +57,7 @@ const CreateJobStep2 = ({ getJobs }) => {
       if (response.status === 201) {
         toast.success(response.statusText);
         setIsModal2Open(false);
-        setCurrentJob(null)
+        setCurrentJob(null);
         getJobs();
       }
     } catch (error) {
@@ -68,6 +68,9 @@ const CreateJobStep2 = ({ getJobs }) => {
   };
 
   const handleUpdate = async (jobId, data) => {
+    if (isLoading) return;
+
+    setIsLoading(true);
     try {
       const response = await axios.put(
         `${process.env.REACT_APP_API_URL}/jobs/${jobId}`,
@@ -76,11 +79,13 @@ const CreateJobStep2 = ({ getJobs }) => {
       if (response.status === 200) {
         toast.success("Job Updated successfully");
         setIsModal2Open(false);
-        setCurrentJob(null)
+        setCurrentJob(null);
         getJobs();
       }
     } catch (error) {
       toast.error("Failed to update job");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -108,127 +113,134 @@ const CreateJobStep2 = ({ getJobs }) => {
   };
 
   return (
-    <Transition appear show={isModal2Open} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        onClose={() => {
-          setIsModal2Open(false);
-          setCurrentJob(null)
-        }}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+    <>
+      {isLoading && (
+        <div className="flex justify-center items-center h-[calc(100vh-100px)]">
+          <h1 className="text-xl font-medium text-black">Loading...</h1>
+        </div>
+      )}
+      <Transition appear show={isModal2Open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => {
+            setIsModal2Open(false);
+            setCurrentJob(null);
+          }}
         >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
-        </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
 
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-[10px] bg-white p-8 align-middle shadow-xl border border-mercury transition-all">
-                <div className="flex justify-between items-center text-shark">
-                  <h1 className="text-xl font-normal leading-7">
-                    Create a job
-                  </h1>
-                  <h3 className="text-base font-medium leading-6">Step 2</h3>
-                </div>
-                <div className="flex justify-between items-end gap-6">
-                  <TextInput
-                    containerClass="w-full"
-                    label="Experience"
-                    ref={experienceMinRef}
-                    defaultValue={currentJob ? currentJob?.experienceMin : ""}
-                    onKeyPress={handleKeyPress}
-                    onPaste={handlePaste}
-                    placeholder="Minimum"
-                  />
-                  <TextInput
-                    containerClass="w-full"
-                    label=""
-                    placeholder="Maximum"
-                    ref={experienceMaxRef}
-                    defaultValue={currentJob ? currentJob?.experienceMax : ""}
-                    onKeyPress={handleKeyPress}
-                    onPaste={handlePaste}
-                  />
-                </div>
-                <div className="flex justify-between items-end gap-6">
-                  <TextInput
-                    containerClass="w-full"
-                    label="Salary"
-                    placeholder="Minimum"
-                    ref={salaryMinRef}
-                    defaultValue={currentJob ? currentJob?.salaryMin : ""}
-                    onKeyPress={handleKeyPress}
-                    onPaste={handlePaste}
-                  />
-                  <TextInput
-                    containerClass="w-full"
-                    label=""
-                    placeholder="Maximum"
-                    ref={salaryMaxRef}
-                    defaultValue={currentJob ? currentJob?.salaryMax : ""}
-                    onKeyPress={handleKeyPress}
-                    onPaste={handlePaste}
-                  />
-                </div>
-                <TextInput
-                  label="Total employee"
-                  placeholder="ex. 100"
-                  ref={totalEmployeeRef}
-                  defaultValue={currentJob ? currentJob?.totalEmployee : ""}
-                  onKeyPress={handleKeyPress}
-                  onPaste={handlePaste}
-                />
-                <div className="mt-6 text-left">
-                  <Label label="Apply type" />
-                  <div className="flex items-center gap-4">
-                    <RadioButton
-                      value={"Quick apply"}
-                      name={"applyType"}
-                      defaultChecked={currentJob?.applyType === "Quick apply"}
-                      onChange={() => {
-                        applyTypeRef.current = "Quick apply";
-                      }}
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-[10px] bg-white p-8 align-middle shadow-xl border border-mercury transition-all">
+                  <div className="flex justify-between items-center text-shark">
+                    <h1 className="text-xl font-normal leading-7">
+                      Create a job
+                    </h1>
+                    <h3 className="text-base font-medium leading-6">Step 2</h3>
+                  </div>
+                  <div className="flex justify-between items-end gap-6">
+                    <TextInput
+                      containerClass="w-full"
+                      label="Experience"
+                      ref={experienceMinRef}
+                      defaultValue={currentJob ? currentJob?.experienceMin : ""}
+                      onKeyPress={handleKeyPress}
+                      onPaste={handlePaste}
+                      placeholder="Minimum"
                     />
-                    <RadioButton
-                      value={"External apply"}
-                      name={"applyType"}
-                      defaultChecked={
-                        currentJob?.applyType === "External apply"
-                      }
-                      onChange={() => {
-                        applyTypeRef.current = "External apply";
-                      }}
+                    <TextInput
+                      containerClass="w-full"
+                      label=""
+                      placeholder="Maximum"
+                      ref={experienceMaxRef}
+                      defaultValue={currentJob ? currentJob?.experienceMax : ""}
+                      onKeyPress={handleKeyPress}
+                      onPaste={handlePaste}
                     />
                   </div>
-                </div>
-                <div className="text-right mt-24">
-                  <Button className="filled_btn" onClick={handleSaveClick}>
-                    Save
-                  </Button>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
+                  <div className="flex justify-between items-end gap-6">
+                    <TextInput
+                      containerClass="w-full"
+                      label="Salary"
+                      placeholder="Minimum"
+                      ref={salaryMinRef}
+                      defaultValue={currentJob ? currentJob?.salaryMin : ""}
+                      onKeyPress={handleKeyPress}
+                      onPaste={handlePaste}
+                    />
+                    <TextInput
+                      containerClass="w-full"
+                      label=""
+                      placeholder="Maximum"
+                      ref={salaryMaxRef}
+                      defaultValue={currentJob ? currentJob?.salaryMax : ""}
+                      onKeyPress={handleKeyPress}
+                      onPaste={handlePaste}
+                    />
+                  </div>
+                  <TextInput
+                    label="Total employee"
+                    placeholder="ex. 100"
+                    ref={totalEmployeeRef}
+                    defaultValue={currentJob ? currentJob?.totalEmployee : ""}
+                    onKeyPress={handleKeyPress}
+                    onPaste={handlePaste}
+                  />
+                  <div className="mt-6 text-left">
+                    <Label label="Apply type" />
+                    <div className="flex items-center gap-4">
+                      <RadioButton
+                        value={"Quick apply"}
+                        name={"applyType"}
+                        defaultChecked={currentJob?.applyType === "Quick apply"}
+                        onChange={() => {
+                          applyTypeRef.current = "Quick apply";
+                        }}
+                      />
+                      <RadioButton
+                        value={"External apply"}
+                        name={"applyType"}
+                        defaultChecked={
+                          currentJob?.applyType === "External apply"
+                        }
+                        onChange={() => {
+                          applyTypeRef.current = "External apply";
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="text-right mt-24">
+                    <Button className="filled_btn" onClick={handleSaveClick}>
+                      Save
+                    </Button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
-        </div>
-      </Dialog>
-    </Transition>
+        </Dialog>
+      </Transition>
+    </>
   );
 };
 
